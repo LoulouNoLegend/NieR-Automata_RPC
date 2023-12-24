144bb19ad7043bda0c4c2fc4f5dc8192d482b9a1
LoulouNoLegend <40952934+LoulouNoLegend@users.noreply.github.com>
2023-12-24 00:43:30 -0500

    Base RPC

--- scripts/exports.py
+++ scripts/exports.py
@@ -1,10 +1,10 @@
 import pefile
 
 exported_functions = []
-pe = pefile.PE('.\\libScePad.dll')
+pe = pefile.PE('.\\steam_api64.dll')
 for entry in pe.DIRECTORY_ENTRY_EXPORT.symbols:
     func = entry.name.decode('utf-8')
-    exported_functions.append(f'#pragma comment(linker,"/export:{func}=libScePad_o.{func},@{entry.ordinal}")')
+    exported_functions.append(f'#pragma comment(linker,"/export:{func}=NieRAutomataRPC.{func},@{entry.ordinal}")')
 
 exported_functions = '\n'.join(exported_functions)
 print(exported_functions)
